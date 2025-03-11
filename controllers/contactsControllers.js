87dd1059ea958d8d01abaa8efe9d3b85dcb1256f
Andrii Diachenko <andreyzp87@gmail.com>
2025-03-11 11:57:34 +0100

    remove unnesessary validation logic

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,9 +1,5 @@
 import * as contactsService from "../services/contactsServices.js";
 import HttpError from "../helpers/HttpError.js";
-import {
-  createContactSchema,
-  updateContactSchema,
-} from "../schemas/contactsSchemas.js";
 
 export const getAllContacts = async (req, res, next) => {
   try {
@@ -42,10 +38,6 @@ export const deleteContact = async (req, res, next) => {
 
 export const createContact = async (req, res, next) => {
   try {
-    const { error } = createContactSchema.validate(req.body);
-    if (error) {
-      throw HttpError(400, error.message);
-    }
     const { name, email, phone } = req.body;
     const result = await contactsService.addContact(name, email, phone);
     res.status(201).json(result);
@@ -56,15 +48,6 @@ export const createContact = async (req, res, next) => {
 
 export const updateContact = async (req, res, next) => {
   try {
-    if (Object.keys(req.body).length === 0) {
-      throw HttpError(400, "Body must have at least one field");
-    }
-
-    const { error } = updateContactSchema.validate(req.body);
-    if (error) {
-      throw HttpError(400, error.message);
-    }
-
     const { id } = req.params;
     const result = await contactsService.updateContact(id, req.body);
     if (!result) {
